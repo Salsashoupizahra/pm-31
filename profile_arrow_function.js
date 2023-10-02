c76b3cfc2bdf529d21e9009417856ce78f2bb045
Salsashoupizahra <141709266+Salsashoupizahra@users.noreply.github.com>
2023-10-02 13:53:13 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const profile=(namaDepan, namaBelakang, email) =>{
+    console.log("Nama : " + namaDepan + " "+ namaBelakang)
+    console.log("Email : "+ email)
+}
+
+profile("salsa","shoupi","salsa@gmail.com")
